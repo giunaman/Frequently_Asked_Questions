3579efe7c429f3bc890221db26d656d1e7b6fa12
Gigi <79722214+giunaman@users.noreply.github.com>
2022-10-05 10:40:51 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.faq_question').click(function() {
+
+        if ($(this).parent().is('.open')) {
+            $(this).closest('.faq').find('.faq_answer_container').slideUp();
+            $(this).closest('.faq').removeClass('open');
+        } else {
+            $('.faq_answer_container').slideUp();
+            $('.faq').removeClass('open');
+            $(this).closest('.faq').find('.faq_answer_container').slideDown();
+            $(this).closest('.faq').addClass('open');
+        }
+
+    });
+});
